9b4b6d0acb55b4545493ce643e8a07dbf12a7922
RichyCarballo <146492125+RichyCarballo@users.noreply.github.com>
2023-11-01 21:41:23 -0400

    Add files via upload

---
+++ p22_u3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace p22_u3 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0, numero;
+
+            do
+            {
+                Console.WriteLine("Ingrese el numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                suma = suma + numero;
+
+
+
+            } while (numero != 0);
+            Console.WriteLine("Su resultado es: " + suma);
+        }
+    }
+}
+    
+
+    
+
