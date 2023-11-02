1bddb56e6fbfe372a0c34cdb402a650a942cbb70
RichyCarballo <146492125+RichyCarballo@users.noreply.github.com>
2023-11-01 21:47:18 -0400

    Add files via upload

---
+++ p23_u3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 10);
+        }
+    }
+}
